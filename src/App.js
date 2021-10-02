3e137ed880363a23eb2a1b7f959b4375a6d0c63a
yroyero <yroyero@gmail.com>
2021-10-02 11:57:41 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+import React from 'react';
+
+class App extends React.Component {
+ 
+  render(){
+    return (
+      <span>Hello word</span>
+    )
+  }
+}
+
+export default App;
